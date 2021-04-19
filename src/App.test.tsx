ac2187ab6e461ae6d61279b400acb848a914f3e9
Luke Dennis <ldennis@homebound.com>
2021-04-19 17:37:00 -0600

    fine-tuning animations

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { xOffsets } from "./animation";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const startButton = screen.getByText(/Start Game/i);
   expect(startButton).toBeInTheDocument();
+});
+
+test("calculate correct offsets for animation", () => {
+  const cases: Record<string, Array<number>> = {
+    "3": [29, 119, 209],
+    "4": [0, 82, 164, 245],
+    "5": [0, 65, 131, 196, 262],
+  };
+  Object.keys(cases).map(count => {
+    expect(xOffsets(375 - 48, 90, +count).map(x => Math.round(x))).toEqual(cases[count]);
+  })
 });
--- src/animation.ts
+++ src/animation.ts
@@ -15,11 +15,18 @@ function translateX(el: Element, val: string) {
   (el as HTMLElement).style.transform = `translateX(${val})`;
 }
 
+export function xOffsets(containerWidth: number, itemWidth: number, count: number) {
+  const adjustedWidth = Math.min(containerWidth / count, itemWidth);
+  const offset = Math.max(0, (containerWidth - (count * itemWidth)) / 2);
+  return [...Array(count)].map((_, n) => offset + (n * adjustedWidth));
+}
+
 export function slideAnimation(prev: Game, game: Game) {
   if(document?.body && prev?.state === GameState.ACTIVE && prev.level !== game.level) {
     let anims = [] as Array<Fn>;
     const { clientWidth } = document.body;
     (["lives", "stars"] as Array<"lives" | "stars">).forEach(key => {
+      // Transition between 0 and 1 stars: fade rather than slide
       if(key === "stars" && prev.stars + game.stars === 1) {
         const star = (getSlide(key, game[key], "img") || [])[0];
         if(game.stars > prev.stars) {
@@ -29,20 +36,24 @@ export function slideAnimation(prev: Game, game: Game) {
           star?.classList.remove("faded");
           anims.push(() => star?.classList.add("faded"));
         }
-      } else if(prev[key] !== game[key]) {
+      }
+      // Everything else: slide stars/bunnies in and out
+      else if(prev[key] !== game[key]) {
         const diff = game[key] - prev[key];
         const images = getSlide(key, game[key], ".images > div");
         let img: HTMLElement | null = null;
+        const from = xOffsets(clientWidth - (24 * 2), 90, prev[key]);
+        const to   = xOffsets(clientWidth - (24 * 2), 90, game[key]);
         images?.forEach((el, n) => {
-          if(diff < 0 && n === 0) img = el.firstChild as HTMLElement;
+          if(diff < 0 && !img) img = el.firstElementChild as HTMLElement;
           if(n < game[key] - diff) {
-            translateX(el, `${50*diff}%`);
-            anims.push(() => translateX(el, "0px"));
+            translateX(el, `${from[n] - to[n]}px`);
           } else {
             translateX(el, `${clientWidth/2}px`);
-            anims.push(() => translateX(el, `0px`));
           }
+          anims.push(() => translateX(el, "0px"));
         });
+        // If going backwards, add temp images to push away
         if(diff < 0) {
           const slide = (getSlide(key, game[key]) || [])[0];
           slide && img && [...Array(0-diff)].forEach((_, n) => {
@@ -56,6 +67,7 @@ export function slideAnimation(prev: Game, game: Game) {
         }
       }
       if(anims.length) {
+        // Trick for one-way CSS transitions
         document.body.classList.add("no-transition");
         setTimeout(() => {
           document.body.classList.remove("no-transition");
